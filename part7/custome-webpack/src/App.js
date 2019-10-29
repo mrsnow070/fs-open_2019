e5ba145498be63f291010cd9c947f2fd682aa5f4
mrsnow070 <mrsnow070@gmail.com>
2019-10-29 11:06:48 +0200

    7.16

--- part7/custome-webpack/src/App.js
+++ part7/custome-webpack/src/App.js
@@ -1,23 +1,11 @@
 import React, { useState } from 'react'
 import './index.scss';
 import './App.css'
-import { useNotes } from './hooks/useNotes';
 
 const App = () => {
-    const [counter, setCounter] = useState(0)
-    const [values, setValues] = useState([])
-    const notes = useNotes(BACKEND_URL);
-
-    const handleClick = () => {
-        setCounter(counter + 1)
-        setValues(values.concat(counter))
-    }
-
     return (
         <div className="container">
-            hello webpack {counter} clicks
-        <button onClick={handleClick} >press</button>
-            <div>{notes.length} notes on server {BACKEND_URL}</div>
+         CUSTOM WEBPACK
         </div>
     )
 }
